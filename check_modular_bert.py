2191182733945170e63d99adb33ae78372b17a04
glitchinthematrix <bhishma.dedhia@gmail.com>
2021-04-20 00:50:45 -0400

    Bugs removed and BERT modular working

--- check_modular_bert.py
+++ check_modular_bert.py
@@ -1,20 +1,21 @@
 import sys
 sys.path.append('./transformers/src/')
 
-from transformers import BertTokenizer,BertLMHeadModelModular, BertConfig, BertLMHeadModel
+from transformers import  BertConfig,  BertTokenizer
+from transformers.models.bert.modeling_modular_bert import BertModelModular
+
 
 import torch
 
-model_dict = {'l':3,'a':[10,12,14],'f':[512,1024,4096],'h':[512,1024,256],'s':['sdp','wma','sdp']}
+model_dict = {'l':3,'a':[2,4,8],'f':[512,1024,4096],'h':[512,1024,256],'s':['sdp','wma','sdp']}
 
 config = BertConfig()
 
 config.from_model_dict(model_dict)
 
-model = BertLMHeadModelModular(config)
-
+model = BertModelModular(config)
+print(model.config)
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 inputs = tokenizer("Hello, my dog is cute", return_tensors = "pt")
 outputs = model(**inputs)
-
-prediction_logits = outputs.prediction_logits
-seq_relationship_logits = output.seq_relationship_logits
+print("Output embeddings shape:",outputs[0].shape[-1])
