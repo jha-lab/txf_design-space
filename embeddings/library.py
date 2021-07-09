526b14e54d8cf7d5dbde3059e7bf26a6c8e338a5
shikhartuli <shikhartuli98@gmail.com>
2021-07-09 17:51:36 -0400

    Added interpolation between graphs

--- embeddings/library.py
+++ embeddings/library.py
@@ -4,11 +4,15 @@
 
 import yaml
 import numpy as np
+
 import itertools
 from tqdm.contrib.itertools import product
+
 from joblib import Parallel, delayed
-from utils import graph_util, embedding_util, print_util as pu
+from copy import deepcopy
 import json
+
+from utils import graph_util, embedding_util, print_util as pu
 
 
 PARALLELIZE = False
@@ -25,7 +29,6 @@ class GraphLib(object):
 		library (list[Graph]): list of all possible graphs within the design space
 		ops_list (list[str]): list of all possible operation blocks in the computational graph
 	"""
-	
 	def __init__(self, design_space=None, dataset: str = None):
 		"""Init GraphLib instance with design_space
 		
@@ -98,14 +101,14 @@ class GraphLib(object):
 		"""Build the GraphLib library
 		
 		Args:
-		    layers_per_stack (int, optional): the number of layers in the stack
-		    create_graphs (bool, optional): creates grapha and adds to library
-		    check_isomorphism (bool, optional): if True, isomorphism is checked 
-		    	for every graph. Default is False, to save compute time.
-		    increasing (bool, optional): if True, only increasing sizes are considered 
-		    	through the network.
-		    heterogeneous_feed_forward (bool, optional): if True, feed forward layers are 
-		    	heterogeneous inside each encoder layer, if "nff" for that layer is greater than 1.
+			layers_per_stack (int, optional): the number of layers in the stack
+			create_graphs (bool, optional): creates grapha and adds to library
+			check_isomorphism (bool, optional): if True, isomorphism is checked 
+				for every graph. Default is False, to save compute time.
+			increasing (bool, optional): if True, only increasing sizes are considered 
+				through the network.
+			heterogeneous_feed_forward (bool, optional): if True, feed forward layers are 
+				heterogeneous inside each encoder layer, if "nff" for that layer is greater than 1.
 		"""
 		def _get_stack(lst: list, repeat: int):
 			return list(itertools.chain.from_iterable(itertools.repeat(x, repeat) for x in lst))
@@ -200,23 +203,206 @@ class GraphLib(object):
 		print(f'{pu.bcolors.OKGREEN}Graphs created{": " + str(count) if not PARALLELIZE else ""}!{pu.bcolors.ENDC} ' \
 			+ f'\n{len(self.library)} graphs within the design space in the library.')
 
-	def interpolate_neighbors(self, graph1: Graph, graph2: Graph, old_layers_per_stack: int, new_layers_per_stack: int):
+	def interpolate_neighbors(self, 
+		graph1: 'Graph', 
+		graph2: 'Graph', 
+		old_layers_per_stack: int, 
+		new_layers_per_stack: int,
+		check_isomorphism=False, 
+		increasing=False,
+		heterogeneous_feed_forward=False):
 		"""Interpolates between two neighbors with finer grained stacks
-		
+
 		Args:
-		    graph1 (Graph): first graph in the library
-		    graph2 (Graph): second graph in the library
-		    old_layers_per_stack (int): old layers per stack
-		    new_layers_per_stack (int): new layers per stack
+			graph1 (Graph): first graph in the library
+			graph2 (Graph): second graph in the library
+			old_layers_per_stack (int): old layers per stack
+			new_layers_per_stack (int): new layers per stack
+			check_isomorphism (bool, optional): if True, isomorphism is checked 
+				for every graph. Default is False, to save compute time.
+			increasing (bool, optional): if True, only increasing sizes are considered 
+				through the network.
+			heterogeneous_feed_forward (bool, optional): if True, feed forward layers are 
+				heterogeneous inside each encoder layer, if "nff" for that layer is greater than 1.
 
 		Returns:
 			interpolants (list): list of Graph objects between graph1 and graph2
 		"""
-		assert new_layers_per_stack < old_layers_per_stack and old_layers_per_stack % new_layers_per_stack == 0, \
+		assert new_layers_per_stack <= old_layers_per_stack and old_layers_per_stack % new_layers_per_stack == 0, \
 			'Old number of layers per stack should be divisible by new number of layers per stack'
 
-		raise NotImplementedError
+		interpolant = []
 
+		stack_mult = old_layers_per_stack // new_layers_per_stack
+
+		smaller_length = min(graph1.model_dict['l'], graph2.model_dict['l'])
+		larger_length = max(graph1.model_dict['l'], graph2.model_dict['l'])
+		different_lengths = smaller_length != larger_length
+		call_different = False
+
+		stack_design_spaces = []
+
+		possible_o_stack = []
+		possible_n_stack = []
+		possible_f_stack = []
+		possible_h_stack = []
+		possible_nff_stack = []
+
+		for stack in range(smaller_length // old_layers_per_stack):
+			# Create a local design space of the course stack
+			stack_design_space = {}
+
+			stack_design_space['hidden_size'] = list(set([graph.model_dict['h'][stack * old_layers_per_stack] \
+				for graph in [graph1, graph2]]))
+
+			stack_design_space['num_heads'] = list(set([graph.model_dict['n'][stack * old_layers_per_stack] \
+				for graph in [graph1, graph2]]))
+
+			stack_design_space['operation_types'] = list(set([graph.model_dict['o'][stack * old_layers_per_stack] \
+				for graph in [graph1, graph2]]))
+
+			stack_design_space['number_of_feed-forward_stacks'] = list(set( \
+				[len(graph.model_dict['f'][stack * old_layers_per_stack]) for graph in [graph1, graph2]]))
+
+			stack_design_space['feed-forward_hidden'] = list(set([graph.model_dict['f'][stack * old_layers_per_stack][0] \
+				for graph in [graph1, graph2]]))
+
+			stack_design_space['operation_parameters'] = {op: [] for op in stack_design_space['operation_types']}
+			operation_parameters = [graph.model_dict['p'][stack * old_layers_per_stack] for graph in [graph1, graph2]]
+			for par in operation_parameters:
+				op = [op for op in stack_design_space['operation_types'] \
+					if par in self.design_space['operation_parameters'][op]]
+				assert len(op) == 1
+				op = op[0]
+				stack_design_space['operation_parameters'][op].append(par)
+
+			stack_design_spaces.extend([stack_design_space] * stack_mult)
+
+			possible_o = list(itertools.product(stack_design_space['operation_types'], repeat=stack_mult))
+
+			if increasing:
+				possible_n = list(itertools.combinations_with_replacement(stack_design_space['num_heads'], stack_mult))
+				possible_h = list(itertools.combinations_with_replacement(stack_design_space['hidden_size'], stack_mult))
+				if not heterogeneous_feed_forward:
+					possible_f = list(itertools.product(stack_design_space['feed-forward_hidden'], repeat=stack_mult))
+				else:
+					possible_f = [list(itertools.combinations_with_replacement(stack_design_space['feed-forward_hidden'], \
+						nff)) for nff in stack_design_space['number_of_feed-forward_stacks']]
+				possible_nff = list(itertools.combinations_with_replacement(
+										stack_design_space['number_of_feed-forward_stacks'], stack_mult))
+			else:
+				possible_n = list(itertools.product(stack_design_space['num_heads'], repeat=stack_mult))
+				possible_h = list(itertools.product(stack_design_space['hidden_size'], repeat=stack_mult))
+				if not heterogeneous_feed_forward:
+					possible_f = list(itertools.product(stack_design_space['feed-forward_hidden'], repeat=stack_mult))
+				else:
+					possible_f = [list(itertools.product(stack_design_space['feed-forward_hidden'], repeat=nff)) \
+						for nff in stack_design_space['number_of_feed-forward_stacks']]
+				possible_nff = list(itertools.product(
+										stack_design_space['number_of_feed-forward_stacks'], repeat=stack_mult))
+
+			possible_o_stack.append(possible_o)
+			possible_n_stack.append(possible_n)
+			possible_h_stack.append(possible_h)
+			possible_f_stack.append(possible_f)
+			possible_nff_stack.append(possible_nff)
+
+		possible_o = list(list(itertools.chain(*o)) for o in itertools.product(*possible_o_stack))
+		possible_n = list(list(itertools.chain(*n)) for n in itertools.product(*possible_n_stack))
+		possible_h = list(list(itertools.chain(*h)) for h in itertools.product(*possible_h_stack))
+		possible_f = list(list(itertools.chain(*f)) for f in itertools.product(*possible_f_stack))
+		possible_nff = list(list(itertools.chain(*nff)) for nff in itertools.product(*possible_nff_stack))
+
+		stacks = smaller_length // new_layers_per_stack
+		count = 0
+
+		def _get_stack(lst: list, repeat: int):
+			return list(itertools.chain.from_iterable(itertools.repeat(x, repeat) for x in lst))
+
+		def create_graph(interpolants, n, h, o, nff, count):
+			possible_p = itertools.product(*[stack_design_spaces[stack]['operation_parameters'][o[stack]] \
+				for stack in range(smaller_length // new_layers_per_stack)])
+			
+			if call_different:
+				possible_p = list(list(itertools.chain(*p)) for p in itertools.product(*[possible_p, \
+				[larger_graph.model_dict['p'][smaller_length:larger_length]]]))
+
+			for p in possible_p:
+				if not heterogeneous_feed_forward:
+					for f in possible_f:
+						model_dict = {'l': stacks * new_layers_per_stack, 'h': _get_stack(list(h), new_layers_per_stack), \
+							'n': _get_stack(list(n), new_layers_per_stack), 'o': _get_stack(list(o), new_layers_per_stack), \
+							'f': _get_stack([[f[stack]] *  nff[stack] for stack in range(stacks)], new_layers_per_stack), \
+							'p': _get_stack(list(p), new_layers_per_stack)}
+						new_graph = Graph(model_dict, self.ops_list, compute_hash=True)
+						count += 1
+						if check_isomorphism:
+							assert new_graph.hash not in [graph.hash for graph in self.library], \
+								f'{pu.bcolors.FAIL}Two graphs found with same hash! ' \
+								+ f'Check if they are isomorphic:{pu.bcolors.ENDC}\n' \
+								+ f'Graph-1: {new_graph.model_dict}\n' \
+								+ f'Graph-2: {graph.model_dict for graph in self.library if graph.hash == new_graph.hash}'
+						interpolants.append(new_graph)
+				else:
+					for f in itertools.product(*[possible_f[nff[stack]-1] for stack in range(stacks)]):
+						model_dict = {'l': stacks * new_layers_per_stack, 'h': _get_stack(list(h), new_layers_per_stack), \
+							'n': _get_stack(list(n), new_layers_per_stack), 'o': _get_stack(list(o), new_layers_per_stack), \
+							'f': _get_stack(list(f), new_layers_per_stack), \
+							'p': _get_stack(list(p), new_layers_per_stack)}
+						new_graph = Graph(model_dict, self.dataset, self.ops_list, compute_hash=True)
+						count += 1
+						if check_isomorphism:
+							assert new_graph.hash not in [graph.hash for graph in self.library], \
+								f'{pu.bcolors.FAIL}Two graphs found with same hash! ' \
+								+ f'Check if they are isomorphic:{pu.bcolors.ENDC}\n' \
+								+ f'Graph-1: {new_graph.model_dict}\n' \
+								+ f'Graph-2: {graph.model_dict for graph in self.library if graph.hash == new_graph.hash}'
+						interpolants.append(new_graph)
+
+			return count
+
+		interpolants = []
+
+		if not PARALLELIZE:
+			for n, h, o, nff in product(possible_n, possible_h, possible_o, possible_nff, \
+				desc=f'Generating transformers with {stacks * new_layers_per_stack} encoder layers'):
+				count = create_graph(interpolants, n, h, o, nff, count)
+				# print(f'\r{pu.human_format(count)}', end='')
+		else:
+			with Parallel(n_jobs=8, prefer='threads', require='sharedmem') as parallel:
+				parallel(delayed(create_graph)(interpolants, n, h, o, nff, count) \
+					for n, h, o, nff in product(possible_n, possible_h, possible_o, possible_nff, \
+						desc=f'Generating transformers with {stacks * new_layers_per_stack} encoder layers'))
+
+		if different_lengths:
+			larger_graph = graph1 if graph1.model_dict['l'] == larger_length else graph2
+			call_different = True
+
+			possible_o = list(list(itertools.chain(*o)) for o in itertools.product(*[possible_o, \
+				[larger_graph.model_dict['o'][smaller_length:larger_length]]]))
+			possible_n = list(list(itertools.chain(*n)) for n in itertools.product(*[possible_n, \
+				[larger_graph.model_dict['n'][smaller_length:larger_length]]]))
+			possible_h = list(list(itertools.chain(*h)) for h in itertools.product(*[possible_h, \
+				[larger_graph.model_dict['h'][smaller_length:larger_length]]]))
+			possible_f = list(list(itertools.chain(*f)) for f in itertools.product(*[possible_f, \
+				larger_graph.model_dict['f'][smaller_length:larger_length]]))
+			possible_nff = list(list(itertools.chain(*nff)) for nff in itertools.product(*[possible_nff, \
+				[[len(larger_graph.model_dict['f'][i]) for i in range(smaller_length, larger_length)]]]))
+
+			stacks = larger_length // new_layers_per_stack
+
+			if not PARALLELIZE:
+				for n, h, o, nff in product(possible_n, possible_h, possible_o, possible_nff, \
+					desc=f'Generating transformers with {stacks * new_layers_per_stack} encoder layers'):
+					count = create_graph(interpolants, n, h, o, nff, count)
+					# print(f'\r{pu.human_format(count)}', end='')
+			else:
+				with Parallel(n_jobs=8, prefer='threads', require='sharedmem') as parallel:
+					parallel(delayed(create_graph)(interpolants, n, h, o, nff, count) \
+						for n, h, o, nff in product(possible_n, possible_h, possible_o, possible_nff, \
+							desc=f'Generating transformers with {stacks * new_layers_per_stack} encoder layers'))
+
+		return interpolants
 
 	def build_embeddings(self, embedding_size: int, algo='MDS', kernel='WeisfeilerLehman', neighbors=10, n_jobs=8):
 		"""Build the embeddings of all Graphs in GraphLib using MDS
@@ -378,7 +564,6 @@ class GraphLib(object):
 				graphLib.library.append(graph)
 
 		return graphLib
-
 
 
 class Graph(object):
