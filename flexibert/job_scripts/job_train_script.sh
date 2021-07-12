aff8860c58a29bccc5dd29184198310d14bf925e
shikhartuli <shikhartuli98@gmail.com>
2021-07-12 15:24:36 -0400

    Added training script

---
+++ flexibert/job_scripts/job_train_script.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# Sript to train (either fine-tune or pre-train and then fine-tune)
+# a given model.
+
+# Author : Shikhar Tuli
+
+task="sst2"
+cluster="tiger"
+id="stuli"
+pretrain="0"
+model_hash=""
+model_name_or_path=""
+epochs=""
+output_dir=""
+
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+ENDC='\033[0m'
+
+Help()
+{
+   # Display Help
+   echo -e "${CYAN}Sript to train (either fine-tune or pre-train and then fine-tune) a given model${ENDC}"
+   echo
+   echo -e "Syntax: source ${CYAN}job_train_script.sh${ENDC} [${YELLOW}flags${ENDC}]"
+   echo "Flags:"
+   echo -e "${YELLOW}-t${ENDC} | ${YELLOW}--task${ENDC} [default = ${GREEN}\"sst2\"${ENDC}] \t\t\t Name of GLUE task"
+   echo -e "${YELLOW}-c${ENDC} | ${YELLOW}--cluster${ENDC} [default = ${GREEN}\"tiger\"${ENDC}] \t\t Selected cluster - adroit, tiger or della"
+   echo -e "${YELLOW}-i${ENDC} | ${YELLOW}--id${ENDC} [default = ${GREEN}\"stuli\"${ENDC}] \t\t\t Selected PU-NetID to email slurm updates"
+   echo -e "${YELLOW}-p${ENDC} | ${YELLOW}--pretrain${ENDC} [default = ${GREEN}\"0\"${ENDC}] \t\t To pre-train the given model"
+   echo -e "${YELLOW}-m${ENDC} | ${YELLOW}--model_hash${ENDC} [default = ${GREEN}\"\"${ENDC}] \t\t Model hash"
+   echo -e "${YELLOW}-n${ENDC} | ${YELLOW}--model_name_or_path${ENDC} [default = ${GREEN}\"\"${ENDC}] \t Model path for fine-tuning"
+   echo -e "${YELLOW}-e${ENDC} | ${YELLOW}--epochs${ENDC} [default = ${GREEN}\"\"${ENDC}] \t\t\t Number of epochs for fine-tuning"
+   echo -e "${YELLOW}-o${ENDC} | ${YELLOW}--output_dir${ENDC} [default = ${GREEN}\"\"${ENDC}] \t\t Output directory to save fine-tuned result"
+   echo -e "${YELLOW}-h${ENDC} | ${YELLOW}--help${ENDC} \t\t\t\t\t Call this help message"
+   echo
+}
+
+while [[ $# -gt 0 ]]
+do
+case "$1" in
+    -t | --task)
+        shift
+        task=$1
+        shift
+        ;;
+    -c | --cluster)
+        shift
+        cluster=$1
+        shift
+        ;;
+    -i | --id)
+        shift
+        id=$1
+        shift
+        ;;
+    -p | --pretrain)
+        shift
+        pretrain=$1
+        shift
+        ;;
+    -m | --model_hash)
+        shift
+        model_hash=$1
+        shift
+        ;;
+    -n | --model_name_or_path)
+        shift
+        model_name_or_path=$1
+        shift
+        ;;
+    -e | --epochs)
+        shift
+        epochs=$1
+        shift
+        ;;
+    -o | --output_dir)
+        shift
+        output_dir=$1
+        shift
+        ;;
+    -h | --help)
+       Help
+       return 1;
+       ;;
+    *)
+       echo "Unrecognized flag $1"
+       return 1;
+       ;;
+esac
+done  
+
+if [[ $cluster == "adroit" ]]
+then
+  cluster_gpu="gpu:tesla_v100:4"
+elif [[ $cluster == "tiger" ]]
+then
+  cluster_gpu="gpu:4"
+elif [[ $cluster == "della" ]]
+then
+  cluster_gpu="gpu:2"
+else
+	echo "Unrecognized cluster"
+	return 1
+fi
+
+job_file="./${task}/job_${model_hash}_.slurm"
+
+cd ..
+
+# Load GLUE dataset using the internet
+python load_glue_dataset.py --task $task
+
+cd job_scripts/
+
+# Create SLURM job script to train surrogate model
+echo "#!/bin/bash" >> $job_file
+echo "#SBATCH --job-name=flexibert_${task}_${model_hash}  # create a short name for your job" >> $job_file
+echo "#SBATCH --nodes=1                                   # node count" >> $job_file
+echo "#SBATCH --ntasks=1                                  # total number of tasks across all nodes" >> $job_file
+echo "#SBATCH --cpus-per-task=16                          # cpu-cores per task (>1 if multi-threaded tasks)" >> $job_file
+echo "#SBATCH --mem-per-cpu=8G                            # memory per cpu-core (4G is default)" >> $job_file
+echo "#SBATCH --gres=${cluster_gpu}                       # number of gpus per node" >> $job_file
+echo "#SBATCH --time=144:00:00                            # total run time limit (HH:MM:SS)" >> $job_file
+echo "#SBATCH --mail-type=all                             # send email" >> $job_file
+echo "#SBATCH --mail-user=${id}@princeton.edu" >> $job_file
+echo "" >> $job_file
+echo "module purge" >> $job_file
+echo "module load anaconda3/2020.7" >> $job_file
+echo "conda activate txf_design-space" >> $job_file
+echo "" >> $job_file
+echo "cd .." >> $job_file
+echo "" >> $job_file
+if [[ $pretrain == "1" ]]
+then
+  echo "python roberta_pretrain.py --model_name_or_path ${model_name_or_path}" >> $job_file
+fi
+echo "python finetune_flexibert.py --model_name_or_path ${model_name_or_path} \
+  --task_name ${task} \
+  --do_train \
+  --do_eval \
+  --save_total_limit 2 \
+  --max_sequence_length 128 \
+  --per_device_train_batch_size 64 \
+  --learning_rate 2e-5 \
+  --num_train_epochs ${epochs} \
+  --overwrite_output_dir \
+  --output_dir ${output_dir}" >> $job_file
+
+sbatch $job_file
