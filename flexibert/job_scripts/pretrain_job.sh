243acdeda688d2c058215ab0444cd3a21f467e13
Bhishma Dedhia <bhishma.dedhia@gmail.com>
2021-07-14 05:20:46 -0400

    Added pretraining job creator

---
+++ flexibert/job_scripts/pretrain_job.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Sript to load GLUE dataset into cache using load_glue_datset.py 
+# and then create a job script that trains a surrogate model for the
+# given task
+
+# Author : Shikhar Tuli
+
+cluster="della"
+id="bdedhia"
+model="fnet_mini"
+
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+ENDC='\033[0m'
+
+Help()
+{
+   # Display Help
+   echo
+   echo -e "Syntax: source ${CYAN}job_creator_script.sh${ENDC} [${YELLOW}flags${ENDC}]"
+   echo "Flags:"
+   echo -e "${YELLOW}-m${ENDC} | ${YELLOW}--model${ENDC} [default = ${GREEN}\"fnet_mini\"${ENDC}] \Selected Model - fnet_mini, fnet_tiny, convbert_mini, convbert_tiny"
+   echo -e "${YELLOW}-c${ENDC} | ${YELLOW}--cluster${ENDC} [default = ${GREEN}\"tiger\"${ENDC}] \t Selected cluster - adroit or della or tiger"
+   echo -e "${YELLOW}-i${ENDC} | ${YELLOW}--id${ENDC} [default = ${GREEN}\"stuli\"${ENDC}] \t\t Selected PU-NetID to email slurm updates"
+   echo -e "${YELLOW}-h${ENDC} | ${YELLOW}--help${ENDC} \t\t\t\t Call this help message"
+   echo
+}
+
+while [[ $# -gt 0 ]]
+do
+case "$1" in
+    -m | --model)
+        shift
+        model=$1
+        shift
+        ;;
+    -c | --cluster)
+        shift
+        cluster=$1
+        shift
+        ;;
+    -i | --id)
+        shift
+        id=$1
+        shift
+        ;;
+    -h | --help)
+       Help
+       return 1;
+       ;;
+    *)
+       echo "Unrecognized flag $1"
+       return 1;
+       ;;
+esac
+done  
+
+if [[ $cluster == "adroit" ]]
+then
+  cluster_gpu="gpu:tesla_v100:4"
+elif [[ $cluster == "tiger" ]]
+then
+  cluster_gpu="gpu:4"
+elif [[ $cluster == "della" ]]
+then
+  cluster_gpu="gpu:4"
+else
+	echo "Unrecognized cluster"
+	return 1
+fi
+
+
+cd ..
+
+cd job_scripts/
+
+job_file="pretrain_${model}.slurm"
+
+# Create SLURM job script to train surrogate model
+echo "#!/bin/bash
+#SBATCH --job-name=flexibert_${task}        # create a short name for your job
+#SBATCH --nodes=1                           # node count
+#SBATCH --ntasks=1                          # total number of tasks across all nodes
+#SBATCH --cpus-per-task=16                  # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=8G                    # memory per cpu-core (4G is default)
+#SBATCH --gres=${cluster_gpu}               # number of gpus per node
+#SBATCH --time=08:00:00                     # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=all                     # send email
+#SBATCH --mail-user=${id}@princeton.edu
+
+module purge
+module load anaconda3/2020.7
+conda activate txf_design-space
+
+cd ..
+
+python pretrain_flexibert.py --model ${model} " > $job_file
+
+sbatch $job_file
---
+++ flexibert/pretrain_flexibert.py
@@ -0,0 +1,90 @@
+
+from roberta_pretraining import pretrain
+import sys
+sys.path.append('../../transformers/src/')
+sys.path.append('../../embeddings/')
+sys.path.append('../../flexibert/')
+import os
+import torch
+from utils import print_util as pu
+import shlex
+from library import Graph, GraphLib
+import argparse
+
+import logging
+logging.disable(logging.INFO)
+logging.disable(logging.WARNING)
+
+
+
+
+def training(dataset_name, seed, output_dir):
+
+	a = "--dataset_name {} \
+	--seed {} \
+	--do_train \
+	--do_eval \
+	--max_seq_length 512 \
+	--line_by_line True \
+	--per_gpu_train_batch_size 64 \
+	--adam_epsilon 1e-6\
+	--learning_rate 1e-4\
+	--warmup_steps 10000\
+	--lr_scheduler_type linear\
+    --overwrite_output_dir \
+    --output_dir {} \
+    ".format(dataset_name, seed, output_dir)
+
+	return shlex.split(a)
+
+
+def main():
+    """Run pretraining
+    """
+    parser = argparse.ArgumentParser(
+        description='Input parameters for pretraining',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--model',
+        metavar='',
+        type=str,
+        help='Model to pretrain',
+        default='fnet_mini')
+
+    args = parser.parse_args()
+
+
+	graphLib = GraphLib.load_from_dataset('../dataset/dataset_test.json')
+
+	seed = 1
+
+	if args.model == 'fnet_mini':
+		
+		model_dict = {'l':4,'o':['l','l','l','l'],'h':[256,256,256,256],'n':[4,4,4,4],'f':[[1024],[1024],[1024],[1024]],'p':['dft','dft','dft','dft']}
+
+	elif args.model == 'fnet_tiny':
+
+		model_dict =  { 'l': 2, 'o': ['l']*2, 'h': [128]*2, 'n': [2]*2, 'f': [[4*128]]*2, 'p': ['dft']*2}
+
+	elif args.model == 'convbert_mini':
+
+		model_dict = {'l':4,'o':['c','c','c','c'],'h':[256,256,256,256],'n':[4,4,4,4],'f':[[1024],[1024],[1024],[1024]],'p':[9,9,9,9]}
+
+	elif args.model == 'convbert_tiny':
+
+		model_dict= { 'l': 2, 'o': ['c']*2, 'h': [128]*2, 'n': [2]*2, 'f': [[4*128]]*2, 'p': [9]*2}
+
+
+	model_graph = graphLib.get_graph(model_dict=model_dict)[0]
+
+	dataset = "cc_news"
+
+	output_dir = "../../models/pretrained/"+str(model_graph.hash)+'/'
+
+	args_train = training(dataset, seed, output_dir)
+
+	# Forcing to train on single GPU
+	#os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+	metrics = pretrain(args_train, model_dict)
+
+	print(f"MLM Accuracy on cc_news is {metrics['eval_accuracy']:0.2f}")
