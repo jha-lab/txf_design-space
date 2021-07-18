fed39d45abd54af8f0409144885385d281a91533
Bhishma Dedhia <bhishma.dedhia@gmail.com>
2021-07-18 04:16:34 -0400

    Bug fixes

--- flexibert/roberta_pretraining.py
+++ flexibert/roberta_pretraining.py
@@ -171,6 +171,7 @@ class DataTrainingArguments:
         },
     )
 
+    '''
     def __post_init__(self):
         if self.dataset_name is None and self.train_file is None and self.validation_file is None:
             raise ValueError("Need either a dataset name or a training/validation file.")
@@ -181,7 +182,7 @@ class DataTrainingArguments:
             if self.validation_file is not None:
                 extension = self.validation_file.split(".")[-1]
                 assert extension in ["csv", "json", "txt"], "`validation_file` should be a csv, a json or a txt file."
-
+    '''
 
 def pretrain(args,model_dict):
     # See all possible arguments in src/transformers/training_args.py
