1b97000dc6ca4fb7cbcdb6e040f0ed12d17d3483
Bhishma P. Dedhia <bdedhia@della-gpu.princeton.edu>
2021-07-26 18:58:29 -0400

    Added job creator scripts

--- flexibert/job_scripts/glue_score.sh
+++ flexibert/job_scripts/glue_score.sh
@@ -80,7 +80,8 @@ job_file="run_glue_${model}.slurm"
 
 # Create SLURM job script to train surrogate model
 echo "#!/bin/bash
-#SBATCH --job-name=glue_${model}        # create a short name for your job
+#SBATCH --job-name=glue_${model}        # create a short name for your job 
+#SBATCH --partition gpu-100
 #SBATCH --nodes=1                           # node count
 #SBATCH --ntasks=1                          # total number of tasks across all nodes
 #SBATCH --cpus-per-task=16                  # cpu-cores per task (>1 if multi-threaded tasks)
--- flexibert/job_scripts/pretrain_job.sh
+++ flexibert/job_scripts/pretrain_job.sh
@@ -81,6 +81,7 @@ job_file="pretrain_${model}.slurm"
 # Create SLURM job script to train surrogate model
 echo "#!/bin/bash
 #SBATCH --job-name=flexibert_${task}        # create a short name for your job
+#SBATCH --partition gpu-100
 #SBATCH --nodes=1                           # node count
 #SBATCH --ntasks=1                          # total number of tasks across all nodes
 #SBATCH --cpus-per-task=16                  # cpu-cores per task (>1 if multi-threaded tasks)
