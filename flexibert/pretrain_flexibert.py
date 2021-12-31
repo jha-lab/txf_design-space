d225d28e1503f3b6fb0d541ed50dde99bfb9a4a0
shikhartuli <shikhartuli98@gmail.com>
2021-12-31 15:14:32 -0500

    Added support for distributed training

--- flexibert/pretrain_flexibert.py
+++ flexibert/pretrain_flexibert.py
@@ -16,7 +16,7 @@ import logging
 #logging.disable(logging.WARNING)
 
 
-def get_training_args(seed, output_dir):
+def get_training_args(seed, output_dir, local_rank):
 
 	a = "--seed {} \
 	--do_train \
@@ -33,7 +33,8 @@ def get_training_args(seed, output_dir):
 	--output_dir {} \
     --overwrite_output_dir \
     --fp16 \
-        ".format( seed, output_dir)
+    --local_rank {} \
+        ".format(seed, output_dir, local_rank)
 
 	return shlex.split(a)
 
@@ -45,49 +46,48 @@ def test(args):
 
 	seed = 1
 
-	if args.model == 'fnet_mini':
+	if args.model_name == 'fnet_mini':
 		
 		model_dict = {'l':4,'o':['l','l','l','l'],'h':[256,256,256,256],'n':[4,4,4,4],'f':[[1024],[1024],[1024],[1024]],'p':['dft','dft','dft','dft']}
 
-	elif args.model == 'fnet_tiny':
+	elif args.model_name == 'fnet_tiny':
 
 		model_dict =  { 'l': 2, 'o': ['l']*2, 'h': [128]*2, 'n': [2]*2, 'f': [[4*128]]*2, 'p': ['dft']*2}
 
-	elif args.model == 'convbert_mini':
+	elif args.model_name == 'convbert_mini':
 
 		model_dict = {'l':4,'o':['c','c','c','c'],'h':[256,256,256,256],'n':[4,4,4,4],'f':[[1024],[1024],[1024],[1024]],'p':[9,9,9,9]}
 
-	elif args.model == 'convbert_tiny':
+	elif args.model_name == 'convbert_tiny':
 
 		model_dict= { 'l': 2, 'o': ['c']*2, 'h': [128]*2, 'n': [2]*2, 'f': [[4*128]]*2, 'p': [9]*2}
 
-	elif args.model == 'fnet_2_256':
+	elif args.model_name == 'fnet_2_256':
 
 		model_dict = { 'l': 2, 'o': ['l']*2, 'h': [256]*2, 'n': [4]*2, 'f': [[4*256]]*2, 'p': ['dft']*2}
 
-	elif args.model == 'fnet_4_128':
+	elif args.model_name == 'fnet_4_128':
 
 		model_dict = { 'l': 4, 'o': ['l']*4, 'h': [128]*4, 'n': [2]*4, 'f': [[4*128]]*4, 'p': ['dft']*4}
 
-	elif args.model == 'convbert_2_256':
+	elif args.model_name == 'convbert_2_256':
 
 		model_dict = { 'l': 2, 'o': ['c']*2, 'h': [256]*2, 'n': [4]*2, 'f': [[4*256]]*2, 'p': [9]*2}
 
-	elif args.model == 'convbert_4_128':
+	elif args.model_name == 'convbert_4_128':
 
 		model_dict = { 'l': 4, 'o': ['c']*4, 'h': [128]*4, 'n': [2]*4, 'f': [[4*128]]*4, 'p': [9]*4}
 
-	elif args.model == 'bert_mini':
+	elif args.model_name == 'bert_mini':
 
 		model_dict = { 'l': 4, 'o': ['sa']*4, 'h': [256]*4, 'n': [4]*4, 'f': [[1024]]*4, 'p': ['sdp']*4}
 
 
 	model_graph = graphLib.get_graph(model_dict=model_dict)[0]
 
-
 	output_dir = "../models/pretrained/"+str(model_graph.hash)+'/'
 
-	args_train = get_training_args(seed, output_dir)
+	args_train = get_training_args(seed, output_dir, args.local_rank)
 
 	# Forcing to train on single GPU
 	#os.environ["CUDA_VISIBLE_DEVICES"]="0"
@@ -104,7 +104,7 @@ def main(args):
 	model_graph, _ = graphLib.get_graph(model_hash=args.model_hash)
 
 	seed = 1
-	args_train = get_training_args(seed, args.output_dir)
+	args_train = get_training_args(seed, args.output_dir, args.local_rank)
 
 	metrics = pretrain(args_train, model_graph.model_dict)
 
@@ -126,11 +126,16 @@ if __name__ == '__main__':
 		type=str,
 		help='path to load the dataset',
 		default='../dataset/dataset_test_bn.json')
-	parser.add_argument('--model',
+	parser.add_argument('--model_name',
 		metavar='',
 		type=str,
 		help='model to pretrain',
 		default='bert_mini')
+	parser.add_argument('--local_rank',
+		metavar='',
+		type=int,
+		help='rank of the process during distributed training',
+		default=-1)
 
 	args = parser.parse_args()
 
