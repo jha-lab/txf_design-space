fab808a0f72f23051104a41f515389f1efe9cb91
shikhartuli <shikhartuli98@gmail.com>
2022-01-29 16:26:31 -0500

    Added heterogeneous encoder

--- transformers/src/transformers/models/bert/modeling_modular_bert.py
+++ transformers/src/transformers/models/bert/modeling_modular_bert.py
@@ -185,7 +185,7 @@ class BertSelfAttentionModular(nn.Module):
         past_key_value=None,
         output_attentions=False,
     ):
-        
+
         mixed_query_layer = self.query(hidden_states)
 
         # If this is instantiated as a cross-attention module, the keys
@@ -280,6 +280,166 @@ class BertSelfAttentionModular(nn.Module):
 
 
         return outputs
+
+
+# Adding a heterogenous attention module
+class BertHeteroAttentionModular(nn.Module):
+    def __init__(self, config, layer_id):
+        super().__init__()
+
+        assert config.from_model_dict_hetero is True, 'Heterogeneous attention only with model_dict_hetero'
+
+        if config.hidden_dim_list[layer_id] % len(config.attention_heads_list[layer_id]) != 0 and not hasattr(config, "embedding_size"):
+            raise ValueError(
+                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
+                f"heads ({config.num_attention_heads})"
+            )
+
+        self.num_attention_heads = len(config.attention_heads_list[layer_id])
+        self.hidden_size = config.hidden_dim_list[layer_id]
+
+        # Fixing attention_head_size to default values for grow-and-prune weight transfer
+        self.attention_head_size = int(self.hidden_size / 2 ** math.floor(math.log(self.num_attention_heads, 2)))
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(self.hidden_size, self.all_head_size)
+        self.key = nn.Linear(self.hidden_size, self.all_head_size)
+        self.value = nn.Linear(self.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+        self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
+        if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
+            self.max_position_embeddings = config.max_position_embeddings
+            self.distance_embedding = nn.Embedding(2 * config.max_position_embeddings - 1, self.attention_head_size)
+
+        self.is_decoder = config.is_decoder
+
+        self.attention_types = [attention.split('_')[0] for attention in config.attention_heads_list[layer_id]]
+        self.sim_types = [attention.split('_')[1] for attention in config.attention_heads_list[layer_id]]
+
+        wma_count = 0
+        for sim_type in self.sim_types:
+            if sim_type == 'wma':
+                setattr(self, f'W{wma_count}', torch.nn.Parameter(torch.FloatTensor(self.attention_head_size,self.attention_head_size).uniform_(-0.1, 0.1)))
+                wma_count += 1
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask=None,
+        head_mask=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        past_key_value=None,
+        output_attentions=False,
+    ):
+        
+        mixed_query_layer = self.query(hidden_states)
+
+        # If this is instantiated as a cross-attention module, the keys
+        # and values come from an encoder; the attention mask needs to be
+        # such that the encoder's padding tokens are not attended to.
+        is_cross_attention = encoder_hidden_states is not None
+
+        if is_cross_attention and past_key_value is not None:
+            # reuse k,v, cross_attentions
+            key_layer = past_key_value[0]
+            value_layer = past_key_value[1]
+            attention_mask = encoder_attention_mask
+        elif is_cross_attention:
+            key_layer = self.transpose_for_scores(self.key(encoder_hidden_states))
+            value_layer = self.transpose_for_scores(self.value(encoder_hidden_states))
+            attention_mask = encoder_attention_mask
+        elif past_key_value is not None:
+            key_layer = self.transpose_for_scores(self.key(hidden_states))
+            value_layer = self.transpose_for_scores(self.value(hidden_states))
+            key_layer = torch.cat([past_key_value[0], key_layer], dim=2)
+            value_layer = torch.cat([past_key_value[1], value_layer], dim=2)
+        else:
+            key_layer = self.transpose_for_scores(self.key(hidden_states))
+            value_layer = self.transpose_for_scores(self.value(hidden_states))
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        if self.is_decoder:
+            # if cross_attention save Tuple(torch.Tensor, torch.Tensor) of all cross attention key/value_states.
+            # Further calls to cross_attention layer can then reuse all cross-attention
+            # key/value_states (first "if" case)
+            # if uni-directional self-attention (decoder) save Tuple(torch.Tensor, torch.Tensor) of
+            # all previous decoder key/value_states. Further calls to uni-directional self-attention
+            # can concat previous decoder key/value_states to current projected key/value_states (third "elif" case)
+            # if encoder bi-directional self-attention `past_key_value` is always `None`
+            past_key_value = (key_layer, value_layer)
+
+        attention_scores_size = query_layer.size()[:-1] + (query_layer.size()[-2],)
+        attention_scores = torch.zeros(*attention_scores_size)
+
+        wma_count = 0
+        for attention_head in range(self.num_attention_heads):
+            if self.sim_types[attention_head] == 'sdp':
+                # Take the dot product between "query" and "key" to get the raw attention scores.
+                attention_scores[:, attention_head, :, :] = torch.matmul(query_layer[:, attention_head, :, :], 
+                    key_layer[:, attention_head, :, :].transpose(-1, -2))
+            elif self.sim_types[attention_head] == 'wma':
+                attention_scores[:, attention_head, :, :] = torch.matmul(torch.matmul(query_layer[:, attention_head, :, :], getattr(self, f'W{wma_count}')), 
+                    key_layer[:, attention_head, :, :].transpose(-1, -2))
+                wma_count += 1
+
+        if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
+
+            seq_length = hidden_states.size()[1]
+            position_ids_l = torch.arange(seq_length, dtype=torch.long, device=hidden_states.device).view(-1, 1)
+            position_ids_r = torch.arange(seq_length, dtype=torch.long, device=hidden_states.device).view(1, -1)
+            distance = position_ids_l - position_ids_r
+            positional_embedding = self.distance_embedding(distance + self.max_position_embeddings - 1)
+            positional_embedding = positional_embedding.to(dtype=query_layer.dtype)  # fp16 compatibility
+
+            if self.position_embedding_type == "relative_key":
+                relative_position_scores = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
+                attention_scores = attention_scores + relative_position_scores
+            elif self.position_embedding_type == "relative_key_query":
+                relative_position_scores_query = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
+                relative_position_scores_key = torch.einsum("bhrd,lrd->bhlr", key_layer, positional_embedding)
+                attention_scores = attention_scores + relative_position_scores_query + relative_position_scores_key
+
+        
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        
+        if attention_mask is not None:
+            # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+
+        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
+
+        if self.is_decoder:
+            outputs = outputs + (past_key_value,)
+
+
+        return outputs
+
 
 #For dft and dct
 class BertLinearAttentionModular(nn.Module):
@@ -445,15 +605,18 @@ class BertAttentionModular(nn.Module):
     def __init__(self, config, layer_id):
         super().__init__()
 
-        if not(config.attention_type[layer_id] == 'sa' or config.attention_type[layer_id] == 'l'):
-            raise ValueError(
-                f"Incorrect attention type specified"
-            )
+        if config.from_model_dict_hetero:
+            self.self = BertHeteroAttentionModular(config, layer_id)
+        else:
+            if not(config.attention_type[layer_id] == 'sa' or config.attention_type[layer_id] == 'l'):
+                raise ValueError(
+                    f"Incorrect attention type specified"
+                )
 
-        if config.attention_type[layer_id] == 'sa':
-            self.self = BertSelfAttentionModular(config, layer_id)
-        elif config.attention_type[layer_id] == 'l':
-            self.self = BertLinearAttentionModular(config, layer_id)
+            if config.attention_type[layer_id] == 'sa':
+                self.self = BertSelfAttentionModular(config, layer_id)
+            elif config.attention_type[layer_id] == 'l':
+                self.self = BertLinearAttentionModular(config, layer_id)
 
         self.output = BertSelfOutputModular(config, layer_id)
         self.pruned_heads = set()
@@ -1030,14 +1193,15 @@ class BertEncoderModular(nn.Module):
         self.config = config
         layer_list = []
 
-        for layer_id in range(config.num_hidden_layers):
-
-            if config.attention_type[layer_id] == 'c':
-                layer_list.append(ConvBertLayerModular(config,layer_id,layer_id == config.num_hidden_layers - 1))
-        
-            else:
-                
+        if config.from_model_dict_hetero:
+            for layer_id in range(config.num_hidden_layers):
                 layer_list.append(BertLayerModular(config,layer_id,layer_id == config.num_hidden_layers - 1))
+        else:
+            for layer_id in range(config.num_hidden_layers):
+                if config.attention_type[layer_id] == 'c':
+                    layer_list.append(ConvBertLayerModular(config,layer_id,layer_id == config.num_hidden_layers - 1))
+                else:
+                    layer_list.append(BertLayerModular(config,layer_id,layer_id == config.num_hidden_layers - 1))
         
         self.layer = nn.ModuleList(layer_list)
 
