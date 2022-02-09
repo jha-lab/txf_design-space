f3c1dc51aa5969333a7dfa8fbb24d9182dd161c0
shikhartuli <shikhartuli98@gmail.com>
2022-02-08 19:00:34 -0500

    Added heterogeneous BERT-Base

--- flexibert/pretrain_flexibert.py
+++ flexibert/pretrain_flexibert.py
@@ -9,6 +9,7 @@ import torch
 
 import shlex
 from library import Graph, GraphLib
+from utils import graph_util
 import argparse
 
 import logging
@@ -101,19 +102,33 @@ def test(args):
 		model_dict = { 'l': 24, 'o': ['sa']*12 + ['l']*12, 'h': [1024]*12 + [512]*12, 'n': [8]*12 + [16]*12, \
 			'f': [[2048, 2048, 2048]]*12 + [[4096]]*12 , 'p': ['sdp']*12 + ['dct']*12}
 
-	if args.model_name != 'flexibert_large':
+	elif args.model_name == 'bert_base_hetero':
+
+		model_dict = {'l': 12, 'o': [['sa_sdp_64']*12]*12, 'h': [768]*12, 'f': [[3072]]*12}
+
+	if args.model_name.endswith('hetero'):
+		model_graph = graph_util.model_dict_to_graph(model_dict)
+		model_hash = graph_util.hash_graph(*model_graph)
+		output_dir = '../models/pretrained/'+model_hash+'/'
+
+	elif args.model_name != 'flexibert_large':
 		model_graph = graphLib.get_graph(model_dict=model_dict)[0]
+		output_dir = "../models/pretrained/"+str(model_graph.hash)+'/'
+
+
 	else:
 		model_graph = Graph(model_dict=model_dict, ops_list=None, compute_hash=True)
-
-	output_dir = "../models/pretrained/"+str(model_graph.hash)+'/'
+		output_dir = "../models/pretrained/"+str(model_graph.hash)+'/'
 
 	args_train = get_training_args(seed, output_dir, args.local_rank)
 
 	# Forcing to train on single GPU
 	#os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-	metrics = pretrain(args_train, model_dict)
+	if args.model_name.endswith('hetero'):
+		metrics, log_history, model = pretrain(args_train, model_dict)
+	else:
+		metrics = pretrain(args_train, model_dict)
 
 	print(f"MLM Loss on cc_news is {metrics['eval_loss']:0.2f}")
 
