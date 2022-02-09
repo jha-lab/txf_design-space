d911d694abb0de3a287d73f1c3f6bcdb727ee8cc
shikhartuli <shikhartuli98@gmail.com>
2022-02-08 19:46:41 -0500

    Updated config based on RoBERTa

--- transformers/src/transformers/models/bert/configuration_bert.py
+++ transformers/src/transformers/models/bert/configuration_bert.py
@@ -128,8 +128,8 @@ class BertConfig(PretrainedConfig):
         hidden_act="gelu",
         hidden_dropout_prob=0.1,
         attention_probs_dropout_prob=0.1,
-        max_position_embeddings=512,
-        type_vocab_size=2,
+        max_position_embeddings=514, # As per RoBERTa's config (old - 512)
+        type_vocab_size=1, # As per RoBERTa's config (old - 2)
         initializer_range=0.02,
         layer_norm_eps=1e-12,
         pad_token_id=0,
