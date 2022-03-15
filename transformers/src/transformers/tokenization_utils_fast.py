ff1fe252b4a541f478707b7699432311fa3a56e4
shikhartuli <shikhartuli98@gmail.com>
2022-03-15 19:39:36 -0400

    Bug fixes

--- transformers/src/transformers/tokenization_utils_fast.py
+++ transformers/src/transformers/tokenization_utils_fast.py
@@ -383,7 +383,7 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
         )
 
         encodings = self._tokenizer.encode_batch(
-            batch_text_or_text_pairs,
+            str(batch_text_or_text_pairs),
             add_special_tokens=add_special_tokens,
             is_pretokenized=is_split_into_words,
         )
