cdde7d1e7b376a9aa7043445f2806cb90806cc39
shikhartuli <shikhartuli98@gmail.com>
2022-11-20 12:07:46 -0500

    Removed SLURM ID

--- flexibert/finetune_flexibert.py
+++ flexibert/finetune_flexibert.py
@@ -162,9 +162,6 @@ class ModelArguments:
     model_name_or_path: str = field(
         metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
-    id: str = field(
-        metadata={"help": "PU-Net ID"}
-    )
     config_name: Optional[str] = field(
         default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
     )
